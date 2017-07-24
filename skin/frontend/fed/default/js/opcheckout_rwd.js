c05fd491b05ed811018dc48d6f547891d6d868e9
Ihor Huliaiev <ihor@it-zen.org>
2017-07-24 06:28:09 +0300

    Minor fixes

--- skin/frontend/fed/default/js/opcheckout_rwd.js
+++ skin/frontend/fed/default/js/opcheckout_rwd.js
@@ -26,20 +26,20 @@
 Checkout.prototype.gotoSection = function (section, reloadProgressBlock) {
     // Adds class so that the page can be styled to only show the "Checkout Method" step
     if ((this.currentStep == 'login' || this.currentStep == 'billing') && section == 'billing') {
-        $j('body').addClass('opc-has-progressed-from-login');
+        jQuery('body').addClass('opc-has-progressed-from-login');
     }
 
     if (reloadProgressBlock) {
         this.reloadProgressBlock(this.currentStep);
     }
     this.currentStep = section;
-    var sectionElement = $('opc-' + section);
+    var sectionElement = jQuery('opc-' + section);
     sectionElement.addClassName('allow');
     this.accordion.openSection('opc-' + section);
 
     // Scroll viewport to top of checkout steps for smaller viewports
     if (Modernizr.mq('(max-width: ' + bp.xsmall + 'px)')) {
-        $j('html,body').animate({scrollTop: $j('#checkoutSteps').offset().top}, 800);
+        jQuery('html,body').animate({scrollTop: jQuery('#checkoutSteps').offset().top}, 800);
     }
 
     if (!reloadProgressBlock) {
