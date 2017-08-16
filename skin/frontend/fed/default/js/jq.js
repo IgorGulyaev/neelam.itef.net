0fd50bf7388071ef99aadb6e940c720b46a8652e
Ihor Huliaiev <ihor@it-zen.org>
2017-08-16 10:45:44 +0300

    Checkout fixes (5)

--- skin/frontend/fed/default/js/jq.js
+++ skin/frontend/fed/default/js/jq.js
@@ -4,4 +4,19 @@ jQuery(document).ready(function ($) {
         $('html').toggleClass('nav-open');
     });
 
+    $(document).on('change', '#s_method_sy_novaposhta_type_WarehouseWarehouse', function () {
+        if ($(this).is(':checked')) {
+            $('#billing:street1').removeClass('required-entry').prop('disabled', true);
+        } else {
+            $('#billing:street1').addClass('required-entry').prop('disabled', false);
+        }
+    });
+    $(document).on('change', '#s_method_sy_novaposhta_type_WarehouseDoors', function () {
+        if ($(this).is(':checked')) {
+            $('#billing:street1').removeClass('required-entry').prop('disabled', true);
+        } else {
+            $('#billing:street1').addClass('required-entry').prop('disabled', false);
+        }
+    });
+
 });
